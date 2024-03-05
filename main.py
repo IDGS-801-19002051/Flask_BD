d209875981fa61c2fc98961fc82224b3cb3cc362
IDGS-801-19002051 <156255910+IDGS-801-19002051@users.noreply.github.com>
2024-03-04 18:54:01 -0600

    SCSDJ 04/03/24 EliminarMod

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for
 from flask import flash
 from flask_wtf.csrf import CSRFProtect
 from flask import g
@@ -66,6 +66,46 @@ def index():
     db.session.add(alum)
     db.session.commit()
   return render_template("index.html", form=create_form)
+
+@app.route("/eliminar",methods={"GET","POST"})
+def eliminar():
+  create_form=forms.UserForm2(request.form)
+  if request.method=="GET":
+    id=request.args.get("id")
+    alum1 = db.session.query(Alumnos).filter(Alumnos.id == id).first()
+    create_form.id.data=request.args.get("id")
+    create_form.nombre.data=alum1.nombre
+    create_form.apaterno.data=alum1.apaterno
+    create_form.email.data=alum1.email
+  if request.method=='POST':
+    id=create_form.id.data
+    alum=Alumnos.query.get(id)
+    db.session.delete(alum)
+    db.session.commit()
+    return redirect(url_for("ABCompleto"))
+  return render_template("eliminar.html", form=create_form)
+
+@app.route("/modificar",methods={"GET","POST"})
+def modificar():
+  create_form=forms.UserForm2(request.form)
+  if request.method=="GET":
+    id=request.args.get("id")
+    alum1 = db.session.query(Alumnos).filter(Alumnos.id == id).first()
+    create_form.id.data=request.args.get("id")
+    create_form.nombre.data=alum1.nombre
+    create_form.apaterno.data=alum1.apaterno
+    create_form.email.data=alum1.email
+  if request.method=='POST':
+    id=create_form.id.data
+    alum1 = db.session.query(Alumnos).filter(Alumnos.id == id).first()
+    alum1.nombre=create_form.nombre.data
+    alum1.apaterno=create_form.apaterno.data
+    alum1.email=create_form.email.data
+    db.session.add(alum1)
+    db.session.commit()
+    return redirect(url_for("ABCompleto"))
+  return render_template("modificar.html", form=create_form)
+
 
 if __name__ =="__main__":
   csrf.init_app(app)
